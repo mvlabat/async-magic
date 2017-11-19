19ef458df40e53708b0b93ad9ec6c438c2bbb2d7
mvlabat <mvlabat@gmail.com>
2017-11-19 16:08:02 +0200

    Return nanosecs since a counter has been spawned

--- server/src/service.rs
+++ server/src/service.rs
@@ -2,7 +2,7 @@ use std::io;
 use tokio_service::Service;
 use tokio_core::reactor::{Handle, Timeout};
 use futures::{future, Future};
-use std::time::Duration;
+use std::time::{Instant, Duration};
 use tokio_timer::{Timer, TimeoutError};
 use futures_cpupool::CpuPool;
 
@@ -31,9 +31,12 @@ impl Service for TimeoutService {
     type Future = Box<Future<Item = Self::Response, Error = Self::Error>>;
 
     fn call(&self, req: Self::Request) -> Self::Future {
-        let task = self.pool.spawn_fn(|| -> Result<Self::Response, Self::Error> {
+        let time_started = Instant::now();
+
+        let task = self.pool.spawn_fn(move || -> Result<Self::Response, Self::Error> {
             for _ in 0..30000000 {};
-            Ok(Some(0))
+            let now = Instant::now().duration_since(time_started);
+            Ok(Some(now.as_secs() * 1_000_000_000 + now.subsec_nanos() as u64))
         });
 
         let timeout = self.timer.timeout(task, self.delay)
